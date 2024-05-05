1b22d74bfc0b0de7024514969de80d6447425e11
Jenya <113167825+Genya10@users.noreply.github.com>
2024-05-05 17:35:15 +0300

    add useEffect with setTimeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Details } from "./Details";
+import cl from "./Details.module.scss";
 
 function App() {
+  const [details, setDetails] = useState({
+    isLoading: true,
+    title: "useEffect",
+    description: "Hello,everybody",
+    buttonText: "Click me",
+  });
 
-const [details, setDetails] = useState({
-  title:'Null',
-  description:'Hello,everybody',
-  buttonText:'Click me'
-})
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDetails((prev) => ({ ...prev, isLoading: false }));
+    }, 2000);
+    return ()=>{
+      clearTimeout(timeout);
+    }
+  });
+ 
 
   return (
-    <div>
-     <Details
-       details={details} setDetails={setDetails}
-      />
+    <div className={cl.container}>
+      <img src="./iam.jpg" width={250} />
+      {details.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Details details={details} setDetails={setDetails} />
+      )}
+      <p className={cl.text}>My third vite app</p>
     </div>
   );
 }
--- src/Details.jsx
+++ src/Details.jsx
@@ -3,8 +3,7 @@ import cl from './Details.module.scss'
 export const Details = ({details, setDetails}) => {
   return (
     <div>
-      <div className={cl.container}>
-        <img src="./iam.jpg" width={250} />
+      <div >       
         <div>
          <h1>{details.title}</h1>
          <h2>{details.description}</h2>
@@ -13,13 +12,13 @@ export const Details = ({details, setDetails}) => {
                  setDetails(prev => {
                     return {
                         ...prev,
-                        title: prev.title + '...'
+                        description: prev.description + '+new'
                     }
                  })
             }}>{details.buttonText}</button>            
          </div>
         </div>
-        <p className={cl.text}>My third vite app</p>
+       
       </div>
     </div>
   );
