e45d550d406d838566eeed5468fcdb8da6d6c04a
Jenya <113167825+Genya10@users.noreply.github.com>
2024-05-07 19:19:34 +0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Details } from "./Details";
 import cl from "./Details.module.scss";
 import { AuthContext } from "./AuthContext";
 import { Header } from "./Header";
+import { Link, useNavigate } from "react-router-dom";
 
 function App() {
+  const [todos, setTodos] = useState([]);
 
-  const {isLoggedIn, setIsLoggedIn} = useAuth();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const data = await response.json();
+      setTodos(data);
+    };
+    fetchData();
+  }, []);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/about-us");
+  }, []);
 
   return (
     <div className={cl.container}>
-      <Header/>
-     
-      {isLoggedIn ? (
-        <button onClick={()=> setIsLoggedIn(false)}>
-          Logout
-        </button>
-      ):(
-        <button onClick={()=> setIsLoggedIn(true)}>
-         Enter
-        </button>
-      )}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+      <Link to="/about-us">Go to about-us</Link>
     </div>
-  )
+  );
 }
 
 export default App;
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './AuthContext.jsx';
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    errorElement:<div>Not found</div>
+  },
+  {
+    path:'/about-us',
+    element:<h2>About us</h2>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-        <App /> 
+       <RouterProvider router={router}/>
     </AuthProvider> 
   </React.StrictMode>,
 )
