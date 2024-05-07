6ea8e677555051e9fbcbf506231b412779b30616
Jenya <113167825+Genya10@users.noreply.github.com>
2024-05-07 21:27:54 +0300

    add fulled index.css

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -4,7 +4,8 @@ import { EmailList } from "./email-list/EmailList";
 export function Home(){
     return <div style={{
         display:'grid',
-        gridTemplateColumns:'1fr .8fr'
+        gridTemplateColumns:'1fr .6fr',
+        padding:'1.5rem',
     }}>
         <EmailEditor/>
         <EmailList/>
