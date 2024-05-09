349108ef965bd2eae756efad1bdf594246278f52
Jenya <113167825+Genya10@users.noreply.github.com>
2024-05-09 20:00:34 +0300

    intall lucide-react and filled EmailEditor.tsx

--- src/component/email-editor/EmailEditor.tsx
+++ src/component/email-editor/EmailEditor.tsx
@@ -1,13 +1,31 @@
 import { useState} from "react";
 import cl from './EmailEditor.module.css';
+import { Eraser,Bold, Italic, Underline } from "lucide-react";
 
 function EmailEditor() {
 
   return (
-    <>
+    <div>
       <h1>Email editor</h1>
-      <div className={cl.card}></div>
-    </>
+      <div className={cl.card}>
+        <div className={cl.editor}>
+         Hey!
+         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Animi, dolor? Saepe, cum repellat sequi voluptates fuga
+          alias soluta voluptas maxime. Natus magnam,
+          voluptate incidunt esse libero sit distinctio ut tempora.
+        </div>
+        <div className={cl.actions}>
+          <div className={cl.tools}>
+          <button><Eraser/></button>
+          <button><Bold/></button>
+          <button><Italic/></button>
+          <button><Underline/></button>
+          </div>
+          <button>Send now</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
